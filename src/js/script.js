c4556eb12bc661ff237d5df581f7b6a2e328615b
takuya <awa08082023@gmail.com>
2024-06-09 07:12:05 +0900

    ハンバーガーメニュー微調整途中まで

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
-
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
-
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+  // ハンバーガーメニュー
+  $(".js-hamburger").on("click", function () {
+    $(this).toggleClass("is-active");
+    $(".js-drawer").fadeToggle("is-active");
+    $(".js-drawer-bg").fadeToggle("is-active");
+  });
 });
